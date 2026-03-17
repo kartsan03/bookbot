62db035aa005c5f2851387c6bc7f7d17e6d9c5be
kartsan03 <kartsan03@gmail.com>
2026-03-17 22:02:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def number_of_words(text):
+	number = len(text.split())
+	return number
+
+def main():
+	path = "books/frankenstein.txt"
+	text = get_book_text(path)
+	count = number_of_words(text)
+	print(f"Found {count} total words")
+
+main()
