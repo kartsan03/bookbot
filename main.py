4ae38cad5dd7dcf505d563529b688f79c9d25cd9
kartsan03 <kartsan03@gmail.com>
2026-03-18 19:31:59 +0100

    get rid of hardcoded path to book

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import number_of_words, number_of_letters, sorted_list
 
 def get_book_text(filepath):
@@ -6,8 +7,11 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
+	if len(sys.argv) < 2 :
+		print ("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	path = sys.argv[1]
 	print("============ BOOKBOT ============")
-	path = "books/frankenstein.txt"
 	print(f"Analyzing book found at {path}")
 	text = get_book_text(path)
 	count = number_of_words(text)
