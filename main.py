532c5c68b79f91f0ee6c0c4245d639f9edf7b42c
kartsan03 <kartsan03@gmail.com>
2026-03-18 19:14:44 +0100

    add report printing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import number_of_words, number_of_letters
+from stats import number_of_words, number_of_letters, sorted_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,10 +6,17 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
+	print("============ BOOKBOT ============")
 	path = "books/frankenstein.txt"
+	print(f"Analyzing book found at {path}")
 	text = get_book_text(path)
 	count = number_of_words(text)
+	print("----------- Word Count ----------")
 	print(f"Found {count} total words")
-	print(number_of_letters(text))
+	print("--------- Character Count -------")
+	for item in sorted_list(number_of_letters(text)):
+		if item["char"].isalpha():
+			print(f"{item['char']}: {item['num']}")
+	print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def number_of_letters(text):
 		else :
 			new_dict[char] = 1
 	return new_dict
+
+def sort_on(items):
+	return items["num"]
+
+def sorted_list(text):
+	finished_dictionary = []
+	for pair in text:
+		finished_dictionary.append({"char": pair, "num": text[pair]})
+	finished_dictionary.sort(key=sort_on, reverse=True)
+	return finished_dictionary
